8fbfba6184547ae5365e579f6f3b8438a83bb7d7
Kevin Garcia <kjg365@nyu.edu>
2018-10-02 01:06:53 -0400

    Milestone

---
+++ Assets/Scripts/Progressbar.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Usage: This script fills the progress bar in relation to player clicks
+// Intent: This script will make a progress bar on the UI that fills up or drains based on how fast the player clicks a button
+
+public class Progressbar : MonoBehaviour {
+
+    // Variables:
+    public float progress;
+    public Vector2 position = new Vector2(20, 40);
+    public Vector2 size = new Vector2(200, 20);
+    public Texture2D progress_empty_image;
+    public Texture2D progress_full_image;
+    public RigidbodyVehicle carscript;
+
+	void OnGUI()
+	{
+        GUI.DrawTexture (new Rect (position.x, position.y, size.x, size.y), progress_empty_image);
+        GUI.DrawTexture(new Rect(position.x, position.y, size.x * progress, size.y), progress_full_image);
+	}
+
+	void Start () 
+    {
+        progress = 0f;
+	}
+	
+	
+	void FixedUpdate () 
+    {
+        if (progress < 1) 
+        {
+            progress -= 0.01f;
+        }
+        progress = Mathf.Clamp01(progress);
+       // Mathf.Max(0, Mathf.Min(1, progress));
+	}
+    public void OnClickDown()
+    {
+
+        if (progress >= 1)
+        {
+            carscript.stalled = false;
+        }
+        else { progress += 0.15f; }
+
+    }
+
+   
+}
--- Assets/Scripts/RigidbodyVehicle.cs
+++ Assets/Scripts/RigidbodyVehicle.cs
@@ -2,35 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RigidbodyVehicle : MonoBehaviour {
+public class RigidbodyVehicle : MonoBehaviour
+{
 
     Rigidbody rbody;
     Vector2 inputVector; // x is horizontal, y is vertical input
     public float turnSpeed;
     public float moveSpeed;
-
-    bool stalled; 
+    public int count;
+    public bool stalled;
+    public float timer;
+    public Progressbar progressbarscript;
 
     // Use this for initialization
     void Start()
     {
         rbody = GetComponent<Rigidbody>();
-        Cursor.visible = false;
+        count = 0;
+        stalled = true;
+        timer = Random.Range(15f, 32f);
+        // Cursor.visible = false;
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
         // input handling
-
         // Input.GetAxis() returns a float between -1f and 1f, and 0 if nothing is happening
         // horizontal = A/D, Left/Right... Left = -1f, Right = +1f
-        float horizontal = Input.GetAxis("Horizontal");
-        // vertical = W/S, Up/Down... Up = +1, Down = -1f
-        float vertical = Input.GetAxis("Vertical");
-
         // put input values into the inputVector, for FixedUpdate
-        inputVector = new Vector2(horizontal, vertical);
+        if (!stalled)
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            // vertical = W/S, Up/Down... Up = +1, Down = -1f
+            float vertical = Input.GetAxis("Vertical");
+            inputVector = new Vector2(horizontal, vertical);
+            timer -= Time.deltaTime;
+
+            if (timer <= 0)
+            {
+                stalled = true;
+                timer = Random.Range(15f, 32f);
+                progressbarscript.progress = 0f;
+            }
+        }
+       
     }
 
     // FixedUpdate runs every physics frame
@@ -38,12 +56,17 @@ public class RigidbodyVehicle : MonoBehaviour {
     void FixedUpdate()
     {
         // forward / backward thrust
-        if (rbody.velocity.magnitude < 4f) // how fast are we going? if too fast, don't speed up
+        //  if (Input.GetKeyDown(KeyCode.G))
+        if (!stalled)
         {
-            rbody.AddForce(transform.forward * inputVector.y * moveSpeed, ForceMode.Impulse);
+            if (rbody.velocity.magnitude < 10f) // how fast are we going? if too fast, don't speed up
+            {
+                rbody.AddForce(transform.forward * inputVector.y * moveSpeed, ForceMode.Impulse);
+            }
+
+            // left/right turning... torque is rotational force
+            rbody.AddTorque(0f, inputVector.x * turnSpeed, 0f);
         }
 
-        // left/right turning... torque is rotational force
-        rbody.AddTorque(0f, inputVector.x * turnSpeed, 0f);
     }
 }
